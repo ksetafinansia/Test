f4f44896185c1043790ec6b398e7bb654add7e48
Rey <artube090@gmail.com>
2022-09-30 17:56:25 +0700

    add label and component to ProductCollectionViewCell

--- Test/CollectionViewCell/CatalogCollectionViewCell.swift
+++ Test/CollectionViewCell/CatalogCollectionViewCell.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class CatalogCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
     
+    @IBOutlet weak var productDescriptionLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         
         // Initialization code
+    }
+    
+    func setupUI(product: Product){
+        productNameLabel.text = product.title
+        priceLabel.text = "\(product.price)"
+        productDescriptionLabel.text = product.description
+        guard let urlString = product.image, let url = URL(string: urlString) else{return}
     }
 
 }
